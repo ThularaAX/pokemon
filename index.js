795f83085ee21963133bf7abdb770a220b4d299b
Thulara <thularablockchain@gmail.com>
2023-11-24 11:32:09 +1100

    Fetch PokeData and render it inside ul,li tag

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log("Hi Pokemon")
+
+async function collectPokemon(){
+    const request = await fetch('https://pokeapi.co/api/v2/pokemon/')
+    const dataList = await request.json()
+    visualisPokeList(dataList.results)
+}
+
+function visualisPokeList(dataListOfPoke){
+    dataListOfPoke.forEach(pokemon =>{
+        const ul = document.querySelector('ul')
+        const li = document.createElement('li')
+        li.innerText = pokemon.name
+        ul.append(li)
+    })
+}
+
+collectPokemon()
