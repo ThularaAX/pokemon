16f70586d8f09c741a33c762d4c8ad7132e7deb6
Thulara <thularablockchain@gmail.com>
2023-11-24 16:21:06 +1100

    Update async function

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-console.log("Hi Pokemon")
+const pokemonDataArray = []
 
-async function collectPokemon(){
+async function collectPokemon() {
     const request = await fetch('https://pokeapi.co/api/v2/pokemon/')
     const dataList = await request.json()
-    visualisPokeList(dataList.results)
+
+    for (const { url } of dataList.results) {
+        const pokemonResponse = await fetch(url)
+        const pokemonData = await pokemonResponse.json()
+
+        pokemonDataArray.push({
+            name: pokemonData.name,
+            image: pokemonData.sprites.front_default,
+        })
+    }
+    visualisPokeList(pokemonDataArray);
 }
 
 function visualisPokeList(dataListOfPoke){
-    dataListOfPoke.forEach(pokemon =>{
+    dataListOfPoke.forEach(PokemonData =>{
         const ul = document.querySelector('ul')
         const li = document.createElement('li')
-        li.innerText = pokemon.name
+        li.innerText = PokemonData.name
         ul.append(li)
     })
 }
